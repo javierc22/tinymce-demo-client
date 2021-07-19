5c70efeeac345921d406849edc0eb05f93359026
javierc22 <javier.ec.22@gmail.com>
2021-07-18 21:06:58 -0400

    Integrar TinyMce

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <editor
+      v-model="content"
+      :api-key="apiKey"
+      :init="{
+        height: 500,
+        menubar: false,
+        selector: 'textarea',
+        file_picker_types: 'image',
+        image_title: true,
+        file_picker_callback: function (cb, value, meta) {
+
+        },
+        plugins: [
+          'advlist autolink lists link image charmap',
+          'searchreplace visualblocks code fullscreen',
+          'print preview anchor insertdatetime media',
+          'paste code help wordcount table'
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic | \
+          alignleft aligncenter alignright | \
+          bullist numlist outdent indent | help | media | link image | code'
+      }"
+    >
+    </editor>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Editor from '@tinymce/tinymce-vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    'editor': Editor
+  },
+  data() {
+    return {
+      content: "",
+      apiKey: process.env.VUE_APP_TINYMCE_API_KEY
+    }
+  },
 }
 </script>
