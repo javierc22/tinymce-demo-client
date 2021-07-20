3c71992e2f3cb776891f294e25b2a8754c049e9d
javierc22 <javier.ec.22@gmail.com>
2021-07-19 22:57:39 -0400

    Agregar componente TinyEditor

---
+++ src/components/TinyEditor.vue
@@ -0,0 +1,57 @@
+<template>
+  <editor
+    v-model="content"
+    :api-key="apiKey"
+    :init="{
+      height: 500,
+      menubar: false,
+      selector: 'textarea',
+      file_picker_types: 'image',
+      image_title: true,
+      file_picker_callback: function (cb, value, meta) {
+
+      },
+      plugins: [
+        'advlist autolink lists link image charmap',
+        'searchreplace visualblocks code fullscreen',
+        'print preview anchor insertdatetime media',
+        'paste code help wordcount table'
+      ],
+      toolbar:
+        'undo redo | formatselect | bold italic | \
+        alignleft aligncenter alignright | \
+        bullist numlist outdent indent | help | media | link image | code'
+    }"
+  >
+  </editor>
+</template>
+<script>
+import Editor from '@tinymce/tinymce-vue'
+
+export default {
+  props: {
+    value: { type: String, default: "" }
+  },
+  components: {
+    'editor': Editor
+  },
+  data() {
+    return {
+      apiKey: process.env.VUE_APP_TINYMCE_API_KEY
+    }
+  },
+  computed: {
+    content: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('setValue', value)
+      }
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
