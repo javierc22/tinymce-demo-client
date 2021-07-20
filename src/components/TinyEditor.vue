0a6a25825e2be4f67dceab2593a0b36c5163cae3
javierc22 <javier.ec.22@gmail.com>
2021-07-19 23:00:46 -0400

    Fix componente TinyEditor

--- src/components/TinyEditor.vue
+++ src/components/TinyEditor.vue
@@ -1,29 +1,32 @@
 <template>
-  <editor
-    v-model="content"
-    :api-key="apiKey"
-    :init="{
-      height: 500,
-      menubar: false,
-      selector: 'textarea',
-      file_picker_types: 'image',
-      image_title: true,
-      file_picker_callback: function (cb, value, meta) {
+  <div>
+    <label class="label">Contenido</label>
+    <editor
+      v-model="content"
+      :api-key="apiKey"
+      :init="{
+        height: 500,
+        menubar: false,
+        selector: 'textarea',
+        file_picker_types: 'image',
+        image_title: true,
+        file_picker_callback: function (cb, value, meta) {
 
-      },
-      plugins: [
-        'advlist autolink lists link image charmap',
-        'searchreplace visualblocks code fullscreen',
-        'print preview anchor insertdatetime media',
-        'paste code help wordcount table'
-      ],
-      toolbar:
-        'undo redo | formatselect | bold italic | \
-        alignleft aligncenter alignright | \
-        bullist numlist outdent indent | help | media | link image | code'
-    }"
-  >
-  </editor>
+        },
+        plugins: [
+          'advlist autolink lists link image charmap',
+          'searchreplace visualblocks code fullscreen',
+          'print preview anchor insertdatetime media',
+          'paste code help wordcount table'
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic | \
+          alignleft aligncenter alignright | \
+          bullist numlist outdent indent | help | media | link image | code'
+      }"
+    >
+    </editor>
+  </div>
 </template>
 <script>
 import Editor from '@tinymce/tinymce-vue'
