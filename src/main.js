6c0099f7ba09426bafbaeada54f9962762077c22
javierc22 <javier.ec.22@gmail.com>
2021-07-21 21:35:04 -0400

    Integrar axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 require("./plugins/buefy")
+import axios from './plugins/axios';
 
+Vue.use(axios);
 Vue.config.productionTip = false
 
 new Vue({
