468b2a116db75d99b425b9d0f0d2361261c7045c
javierc22 <javier.ec.22@gmail.com>
2021-07-25 15:49:06 -0400

    Mostrar publicaciones en "/"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <section class="section">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+
+    <div class="columns">
+      <div class="column is-12 has-text-right">
+        <b-button 
+          type="is-success" 
+          outlined
+          tag="router-link"
+          to="/nuevo"
+        >Crear publicación</b-button>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-two-fifths" v-for="(post, i) in posts" :key="i">
+        <div class="card">
+          <div class="card-image">
+            <!-- <figure class="image is-16by9">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure> -->
+          </div>
+          <div class="card-content">
+            <p class="title is-4">{{ post.title }}</p>
+
+            <div class="content">
+              <p v-html="post.extract"></p>
+              <br>
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
