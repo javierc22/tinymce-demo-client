4b2c7bafb07cc4a5f20549861eb0ce4b584d4108
javierc22 <javier.ec.22@gmail.com>
2021-08-14 23:51:16 -0400

    tests component PostCard

--- src/tests/helpers/data.js
+++ src/tests/helpers/data.js
@@ -4,7 +4,7 @@ export const postsData = [
     title: "Lorem ipsum dolor sit amet 1",
     content:
     "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus dui nec turpis laoreet venenatis. Nullam et posuere erat, non ultricies odio. Sed sagittis, erat ut maximus viverra, mi enim convallis nibh, consectetur feugiat risus velit ut massa. Praesent sodales dui non ex pretium, vitae laoreet risus malesuada. Mauris vitae sollicitudin est, ac tincidunt leo.</p>\n<p>&nbsp;</p>\n<p><strong>Vivamus commodo felis sit amet interdum eleifend. Maecenas porttitor urna mauris, nec mollis quam tempus vitae. Nulla lacinia leo tristique nisl iaculis aliquet.</strong></p>\n<p>&nbsp;</p>\n<p><img src=\"https://cdn.pixabay.com/photo/2021/07/18/17/43/lake-6476212_960_720.jpg\" alt=\"\" width=\"440\" height=\"293\" /></p>\n<p>&nbsp;</p>\n<p>&nbsp;</p>",
-    extract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus ",
+    extract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus",
   },
   {
     id: 2,
---
+++ src/tests/unit/components/PostCard.spec.js
@@ -0,0 +1,54 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import { postsData } from "../../helpers/data"
+import PostCard from '@/components/PostCard'
+
+jest.mock('@/api/posts')
+
+describe('PostCard component', () => {
+  let wrapper = null
+
+  beforeEach(() => {
+    wrapper = mount(PostCard, {
+      propsData: { post: postsData[0] }
+    })
+  })
+
+  it('should show information about post', () => {
+    expect(wrapper.props().post).toEqual(postsData[0])
+    expect(wrapper.findAll('p').at(0).text()).toMatch(postsData[0].title)
+    expect(wrapper.findAll('p').at(1).text()).toMatch(postsData[0].extract)
+  });
+
+  it('should have props post', async() => {
+    const postProp = wrapper.vm.$options.props.post
+    expect(postProp.type).toBe(Object)
+    expect(postProp.required).toBeTruthy()
+  })
+})
+
+describe('Events PostCard Components', () => {
+  let wrapper = null
+  
+  const $route = {
+    name: "/1"
+  }
+
+  const $router = {
+    push: jest.fn(),
+  }
+
+  beforeEach(() => {
+    wrapper = shallowMount(PostCard, {
+      propsData: { post: postsData[0] },
+      mocks: {
+        $route,
+        $router,
+      }
+    })
+  })
+
+  it('should redirect /:id when click in PostCard', async() => {
+    await wrapper.trigger('click')
+    expect(wrapper.vm.$route.name).toBe(`/1`)
+  });
+})
