b30dac403a8d8307e06b7815cf9fc01fd6402f09
javierc22 <javier.ec.22@gmail.com>
2021-09-05 18:54:43 -0300

    Fix base URL production

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = process.env.VUE_APP_BASE_URL || "http://localhost:3000/api/v1"
+
 const axiosCLient = axios.create({
-  baseURL: "http://localhost:3000/api/v1"
+  baseURL
 })
 
 export async function fetchPosts() {
